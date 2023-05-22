2fe8d20092ef444a0daac1c769cfcc070fa73b37
AjShirin <shirinajmimc98@gmail.com>
2023-05-22 14:26:27 +0400

    updating doing the security and corn

--- src/main/java/com/codeline/Olympics/Olympics_API/OlympicsApiApplication.java
+++ src/main/java/com/codeline/Olympics/Olympics_API/OlympicsApiApplication.java
@@ -2,8 +2,14 @@ package com.codeline.Olympics.Olympics_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication // use to annotate the main class of a Spring Boot application.
+@EnableWebSecurity // used to enable Spring Security in a web application.
+@EnableScheduling //enable scheduling
+@EnableGlobalMethodSecurity(prePostEnabled = true) // enable security for pre-authorized and post-authorized
 public class OlympicsApiApplication {
 
 	public static void main(String[] args) {
